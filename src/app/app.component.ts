54b0a3702d97440c7e0235dcc9962da63ebb1046
dmirak <dmirakian1023@gmail.com>
2022-12-03 12:49:14 -0500

    pwa check for updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private swUpdate: SwUpdate, private platform: Platform) {
+    this.checkForUpdates();
+  }
+
+  async checkForUpdates(): Promise<void> {
+    if (await this.swUpdate.checkForUpdate()) {
+      console.log('New version found');
+      if (confirm('A new version is available. Load it?')) {
+        try {
+          await this.swUpdate.activateUpdate();
+          confirm('New version activated!');
+          window.location.reload();
+        } catch {
+          console.log('Failed to activate new version');
+        }
+      }
+    } else {
+      console.log('No new version found');
+    }
+  }
 }
