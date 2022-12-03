38ccecbd6300d41b57e74afdd17c6e974511f381
dmirak <dmirakian1023@gmail.com>
2022-12-03 12:00:21 -0500

    adds firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'recipe-maker-b2198',
+    appId: '1:390592823720:web:c0e6cedf5ae9b2738c9d9d',
+    storageBucket: 'recipe-maker-b2198.appspot.com',
+    apiKey: 'AIzaSyD139MO_hLYUQR_-SAsLufsSwCzdzUJ40o',
+    authDomain: 'recipe-maker-b2198.firebaseapp.com',
+    messagingSenderId: '390592823720',
+    measurementId: 'G-8YHP0MFBRL',
+  },
   production: false
 };
 
