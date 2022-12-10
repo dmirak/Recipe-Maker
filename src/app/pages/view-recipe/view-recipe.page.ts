35f338d7f1b112955dd6b3271c44cc2bbbf6ff36
richmondy2000 <richyevu2000@gmail.com>
2022-12-10 13:32:17 -0500

    updated code for view-recipe

--- src/app/pages/view-recipe/view-recipe.page.ts
+++ src/app/pages/view-recipe/view-recipe.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
 
