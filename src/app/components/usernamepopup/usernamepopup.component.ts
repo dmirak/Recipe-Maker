5a6f95097a5e1cabd9b53d1cd4e5de7f74dca690
richmondy2000 <richyevu2000@gmail.com>
2022-12-14 03:14:45 -0500

    update code for usernamepopup

--- src/app/components/usernamepopup/usernamepopup.component.ts
+++ src/app/components/usernamepopup/usernamepopup.component.ts
@@ -1,24 +1,16 @@
-import { Component, Inject } from '@angular/core';
-import { ModalController, NavParams } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-usernamepopup',
   templateUrl: './usernamepopup.component.html',
   styleUrls: ['./usernamepopup.component.scss'],
 })
-export class UsernamepopupComponent {
-  username: string;
-
-  constructor(
-    private modalCtrl: ModalController,
-    private navParams: NavParams,
-    @Inject('username') private usernameService: { username: string }
-  ) {
-    this.username = navParams.get('username') || '';
-  }
+export class UsernamepopupComponent{
+  username!: string;
+  usernameSaved = false;
 
   saveUsername() {
-    this.usernameService.username = this.username;
-    this.modalCtrl.dismiss();
+    localStorage.setItem('userName', this.username);
+    this.usernameSaved = true;
   }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,15 +6,18 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { RecipeComponentModule } from '../../components/recipe/recipe.module';
+import { UsernamepopupComponentModule } from "../../components/usernamepopup/usernamepopup.module";
+import { UsernamepopupComponent } from 'src/app/components/usernamepopup/usernamepopup.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RecipeComponentModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+    declarations: [HomePage, UsernamepopupComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RecipeComponentModule,
+        HomePageRoutingModule,
+        UsernamepopupComponentModule
+    ]
 })
 export class HomePageModule { }
